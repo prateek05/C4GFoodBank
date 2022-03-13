9bf41ddcbd60b0ad34062557f4192fa0194e7715
vnesarikar3 <vin@pillar.app>
2022-03-13 15:41:32 -0400

    Linted

--- app/campaigns/views.py
+++ app/campaigns/views.py
@@ -4,18 +4,19 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from geopy.geocoders import Nominatim
 from rest_framework import mixins, status, viewsets
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response as APIResponse
 
 from campaigns.models import Campaign, Response
 from campaigns.serializers import SurveySerializer
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication 
+
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
-
     def enforce_csrf(self, request):
         return  # To not perform the csrf check previously happening
+
 
 def is_valid_uuid(value):
     try:
@@ -51,7 +52,6 @@ get_response = openapi.Response("survey get response", SurveySerializer)
 @api_view(["GET", "POST"])
 @authentication_classes((CsrfExemptSessionAuthentication, BasicAuthentication))
 def survey(request, campaign_id, site_id) -> APIResponse:
-    
 
     if request.method == "GET":
         if (
@@ -79,19 +79,21 @@ def survey(request, campaign_id, site_id) -> APIResponse:
             for response in request_body:
                 if (
                     "coordinates" in response
-                    and response.get("coordinates") != "" and response.get("coordinates") != None
+                    and response.get("coordinates") != ""
+                    and response.get("coordinates") != None
                 ):
-                    response.update({
-                        "location":
-                        geolocator.reverse(response.get("coordinates")).raw[
-                            "address"
-                        ]["county"]
-                    })
+                    response.update(
+                        {
+                            "location": geolocator.reverse(
+                                response.get("coordinates")
+                            ).raw["address"]["county"]
+                        }
+                    )
                     del response["coordinates"]
                 else:
                     response.update({"location": None})
                     del response["coordinates"]
-                response.update({"site_id":site_id})
+                response.update({"site_id": site_id})
                 print(response)
                 Response.objects.create(**response)
             return APIResponse(status=status.HTTP_204_NO_CONTENT)
