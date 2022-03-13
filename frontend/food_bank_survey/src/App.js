0896b4d928919662cc98d8519d3871119ab5794a
Prahalad Varma <prasanvar@gmail.com>
2022-03-12 23:32:23 -0600

    Adding FE with dummy get-API

--- frontend/food_bank_survey/src/App.js
+++ frontend/food_bank_survey/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Survey from "./components/Survey";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route path='*' element={<SignIn />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/survey/:campaing/:site" element={<Survey />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/food_bank_survey/src/components/Survey.jsx
@@ -0,0 +1,177 @@
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  Paper,
+  TextField,
+  Button,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+export default function Survey() {
+  const paperStyle = {
+    padding: 20,
+    // height: "vh",
+    width: "80%",
+    margin: "50px auto",
+  };
+  const [location, setLocation] = useState({
+    loaded: false,
+    error: false,
+    coords: { lat: "", lng: "" },
+  });
+
+  const [campaign, setCampaign] = useState([]);
+  const [currentQue, setCurrentQue] = useState({});
+  const [currentQueNum, setCurrentQueNum] = useState(0);
+  const [response, setResponse] = useState([]);
+  const [currentAns, setCurrentAns] = useState({});
+
+  const [completeFlag, setCompleteFlag] = useState(false);
+
+  const initSurvey = async () => {
+    const api = await fetch(
+      `https://22b386a2-2e76-45e8-8e4e-6cc4145b36d6.mock.pstmn.io/api/survey/a242e0ba-e263-4578-832d-596fb9ae7d20/8fafcd51-e079-4bea-b90f-1416483c9115`
+    );
+    const data = await api.json();
+    setCampaign(data);
+    if (data) {
+      setCurrentQueNum(0);
+      setCurrentQue(data[0]);
+      console.log(currentQue);
+    }
+  };
+  const gotoNextQue = (e) => {
+    e.preventDefault();
+    debugger;
+    const loc = location.coords.lat + ", " + location.coords.lng;
+    const answer = {
+      question_id: currentQue.question_id,
+      value: currentAns.value,
+      language: currentQue.language,
+      coordinate: loc,
+    };
+
+    setResponse([answer, ...response]);
+    const nextNum = currentQueNum + 1;
+    if (nextNum < campaign.length) {
+      setCurrentQueNum(nextNum);
+      setCurrentQue(campaign[nextNum]);
+    } else {
+      // Call the post here
+      setCompleteFlag(true);
+    }
+  };
+  const onLocSuccess = (location) => {
+    setLocation({
+      loaded: true,
+      error: false,
+      coords: {
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      },
+    });
+  };
+  const onLocError = (err) => {
+    setLocation({
+      loaded: true,
+      error: true,
+    });
+  };
+
+  const handleTextFieldInput = (e) => {
+    setCurrentAns({ value: e.target.value });
+  };
+
+  useEffect(() => {
+    if (!("geolocation" in navigator)) {
+      onLocError("error");
+    }
+    navigator.geolocation.getCurrentPosition(onLocSuccess, onLocError);
+  }, []);
+
+  useEffect(() => {
+    initSurvey();
+  }, []);
+  return (
+    <Grid
+      container
+      spacing={{ xs: 0, sm: 0, md: 0 }}
+      columns={{ xs: 2, sm: 8, md: 12 }}
+      align="center"
+    >
+      <Grid item xs={12} sm={12} md={12}>
+        {!completeFlag && (
+          <Paper style={{ height: "75vh", ...paperStyle }} elevation={10}>
+            <Grid align="center" style={{ height: "15vh" }}>
+              <h2>{currentQue.question}</h2>
+            </Grid>
+            <Grid
+              container
+              align="center"
+              spacing={1}
+              style={{ height: "25vh" }}
+            >
+              {currentQue.answer_template === "text" && (
+                <TextField
+                  label=""
+                  placeholder="Enter your response"
+                  fullWidth
+                  variant="standard"
+                  value={currentAns.value}
+                  onChange={handleTextFieldInput}
+                />
+              )}
+              {currentQue.answer_template === "radio" && (
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  name="radio-buttons-group"
+                  onChange={(e) => setCurrentAns({ value: e.target.value })}
+                >
+                  {currentQue.answer_choices.split(",").map((choice, index) => {
+                    return (
+                      <FormControlLabel
+                        value={choice}
+                        key={index}
+                        control={<Radio />}
+                        label={choice}
+                      />
+                    );
+                  })}
+                </RadioGroup>
+              )}
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Button
+                onClick={gotoNextQue}
+                variant="contained"
+                endIcon={<SendIcon />}
+              >
+                Continue
+              </Button>
+            </Grid>
+          </Paper>
+        )}
+        {completeFlag && (
+          <Paper style={{ height: "75vh", ...paperStyle }} elevation={10}>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <h1>Thank you for the response!</h1>
+            </Grid>
+          </Paper>
+        )}
+      </Grid>
+    </Grid>
+  );
+}
