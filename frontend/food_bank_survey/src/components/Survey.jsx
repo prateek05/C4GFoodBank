6cdec0e6a23b06c971321b12f04e2a393b6e8077
Prahalad Varma <prasanvar@gmail.com>
2022-03-26 19:05:49 -0500

    Response API called after each answer
    - Changed API calls to be after each answer
    - Added disabled for the continue button in case of no answer

--- frontend/food_bank_survey/src/components/Survey.jsx
+++ frontend/food_bank_survey/src/components/Survey.jsx
@@ -14,7 +14,6 @@ import SendIcon from "@mui/icons-material/Send";
 
 export default function Survey() {
   const api = axios.create({
-
     // baseURL: `https://22b386a2-2e76-45e8-8e4e-6cc4145b36d6.mock.pstmn.io/api/survey/`,
 
     baseURL: `http://ec2-3-91-49-197.compute-1.amazonaws.com/api/survey/`,
@@ -36,11 +35,12 @@ export default function Survey() {
   const [campaign, setCampaign] = useState([]);
   const [currentQue, setCurrentQue] = useState({});
   const [currentQueNum, setCurrentQueNum] = useState(0);
-  const [response, setResponse] = useState([]);
+
   const [currentAns, setCurrentAns] = useState({ value: "" });
   const [dataLoad, setDataLoad] = useState(false);
   const [error, setError] = useState(false);
   const [completeFlag, setCompleteFlag] = useState(false);
+  const [disable, setDisable] = useState(true);
 
   const initSurvey = async () => {
     api
@@ -75,18 +75,13 @@ export default function Survey() {
 
     const nextNum = currentQueNum + 1;
     if (nextNum < campaign.length) {
-      setResponse([answer, ...response]);
       setCurrentQueNum(nextNum);
       setCurrentQue(campaign[nextNum]);
       setCurrentAns({ value: "" });
     } else {
       setCompleteFlag(true);
-      const finalAnswer = [answer, ...response];
-      api.post(
-        campaignId + `/` + siteId,
-        finalAnswer
-      );
     }
+    api.post(campaignId + `/` + siteId, answer);
   };
   const onLocSuccess = (location) => {
     setLocation({
@@ -123,6 +118,10 @@ export default function Survey() {
   useEffect(() => {
     initSurvey();
   }, []);
+  useEffect(() => {
+    currentAns.value !== "" ? setDisable(false) : setDisable(true);
+  }, [currentAns]);
+
   return (
     <Grid
       container
@@ -190,6 +189,7 @@ export default function Survey() {
               <Button
                 onClick={gotoNextQue}
                 variant="contained"
+                disabled={disable}
                 endIcon={<SendIcon />}
               >
                 Continue
